aee6a119f882c2210aec3c6bbf6a1d56b2883068
Rachael <rachaelodejobi@gmail.com>
2024-10-20 00:44:22 +0500

    added increase/decrease functionality for item selection

--- src/components/Cartitem.jsx
+++ src/components/Cartitem.jsx
@@ -4,7 +4,7 @@ import {IoMdAdd, IoMdClose, IoMdRemove} from 'react-icons/io';
 import { CartContext } from "../contexts/CartContext";
 
 const Cartitem = ({item}) => {
- const { removeFromCart, increaseAmount } = useContext(CartContext)
+ const { removeFromCart, increaseAmount, decreaseAmount } = useContext(CartContext)
   const { id, title, image, price, amount } = item;
   return (
     <div className="flex gap-x-4 py-2 lg:px-6 border-b border-gray-200 w-full font-light text-gray-500">
@@ -28,7 +28,7 @@ const Cartitem = ({item}) => {
  {/* qty */}
     <div className="flex flex-1 max-w-[100px] items-center h-full border text-purple-800 font-medium">
  {/* minus icon*/}
-     <div className="flex-1 flex justify-center items-center cursor-pointer">
+     <div onClick={()=> decreaseAmount(id)} className="flex-1 flex justify-center items-center cursor-pointer h-full">
      <IoMdRemove/>
    </div>
   {/* amount*/}
